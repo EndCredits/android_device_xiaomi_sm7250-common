44e60a99dbfcc32d0775bffa6696d576c0144698
johnmart19 <johnivan19999@gmail.com>
2022-07-23 18:11:35 +0300

    sm8250-common: Wifi: Update for vendor.qti.hardware.wifi.supplicant@2.2
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- lito.mk
+++ lito.mk
@@ -486,7 +486,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     libwpa_client \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
