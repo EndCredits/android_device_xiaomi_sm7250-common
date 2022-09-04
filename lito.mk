fe67db268f51c6e666bc776d8b0e5d0ad8cf3615
mikairyuu <miva200267@gmail.com>
2022-09-04 16:59:23 +0300

    sm7250-common: Build audiohalext interface

--- lito.mk
+++ lito.mk
@@ -99,7 +99,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl \
+    vendor.qti.hardware.audiohalext@1.0.vendor
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
