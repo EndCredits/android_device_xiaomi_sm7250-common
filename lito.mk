a4720fc6d3fa1fa4a5aa15b2a21a391e493dee23
EndCredits <alicization.han@gmail.com>
2022-10-26 00:15:27 +0800

    picasso: Remove adb-keys
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- lito.mk
+++ lito.mk
@@ -16,27 +16,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
-PRODUCT_ADB_KEYS := $(LOCAL_PATH)/adb-keys/adb_vendor_key.pub
-
-ifeq ($(wildcard $(PRODUCT_ADB_KEYS)),)
-  $(warning ========================)
-  $(warning The adb key for this release)
-  $(warning )
-  $(warning   $(PRODUCT_ADB_KEYS))
-  $(warning )
-  $(warning does not exist. Most likely PLATFORM_VERSION in build/core/version_defaults.mk)
-  $(warning has changed and a new adb key needs to be generated.)
-  $(warning )
-  $(warning Please run the following commands to create a new key:)
-  $(warning )
-  $(warning   make -j8 adb)
-  $(warning   LOGNAME=android-eng HOSTNAME=google.com adb keygen $(patsubst %.pub,%,$(PRODUCT_ADB_KEYS)))
-  $(warning )
-  $(warning and upload/review/submit the changes)
-  $(warning ========================)
-  $(error done)
-endif
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
