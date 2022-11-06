6fa9aef76f44ac7eb0c559c266d6e26f326c0706
EndCredits <alicization.han@gmail.com>
2022-11-06 23:45:34 +0800

    Revert "sm7250-common: Make fingerprint HIDL fully treble compliant"
    This reverts commit 5094101ab53ebd436663fe7f02b428951264d324.

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2018-2020 The LineageOS Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 
 #include "BiometricsFingerprint.h"
 
-#include <android-base/strings.h>
 #include <cutils/properties.h>
 #include <hardware/fingerprint.h>
 #include <hardware/hardware.h>
@@ -201,17 +200,11 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
         ALOGE("Bad path length: %zd", storePath.size());
         return RequestStatus::SYS_EINVAL;
     }
-    std::string mutableStorePath = storePath;
-    if (android::base::StartsWith(mutableStorePath, "/data/system/users/")) {
-        mutableStorePath = "/data/vendor_de/";
-        mutableStorePath +=
-            static_cast<std::string>(storePath).substr(strlen("/data/system/users/"));
-    }
-    if (access(mutableStorePath.c_str(), W_OK)) {
+    if (access(storePath.c_str(), W_OK)) {
         return RequestStatus::SYS_EINVAL;
     }
 
-    return ErrorFilter(mDevice->set_active_group(mDevice, gid, mutableStorePath.c_str()));
+    return ErrorFilter(mDevice->set_active_group(mDevice, gid, storePath.c_str()));
 }
 
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
