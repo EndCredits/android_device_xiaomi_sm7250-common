af1b256b408782ebfdce5d254d3cc12d0c6e7df3
EndCredits <alicization.han@gmail.com>
2022-11-11 21:13:41 +0800

    sm7250-common: Build 32-bit interfaces for codec2 HAL
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- lito.mk
+++ lito.mk
@@ -276,8 +276,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor:32
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_v1.xml \
