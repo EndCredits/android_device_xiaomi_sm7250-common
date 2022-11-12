e7532565c3e2d5262c53a508a9aab993b53c67d8
EndCredits <alicization.han@gmail.com>
2022-11-12 17:34:11 +0800

    sm7250-common: Enable stagefright codec
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- properties.mk
+++ properties.mk
@@ -287,25 +287,18 @@ ro.config.low_ram = false
 
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
-debug.stagefright.omx_default_rank=512 \
-vendor.media.omx=0 \
-vendor.qc2.venc.avgqp.enable=1 \
-debug.stagefright.c2inputsurface=-1 \
-media.stagefright.enable-player=true \
-media.stagefright.enable-http=true \
-media.stagefright.enable-aac=true \
-media.stagefright.enable-qcp=true \
-media.stagefright.enable-fma2dp=true \
-media.stagefright.enable-scan=true \
-mmp.enable.3g2=true \
+debug.stagefright.ccodec=1 \
 media.stagefright.thumbnail.prefer_hw_codecs=true \
-media.aac_51_output_enabled=true \
-mm.enable.smoothstreaming=true \
-mm.enable.qcom_parser=16711679 \
-persist.mm.enable.prefetch=true
-
-PRODUCT_SYSTEM_PROPERTIES += \
-media.settings.xml=/vendor/etc/media_profiles_vendor.xml
+debug.stagefright.omx_default_rank=0 \
+media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+media.stagefright.enable-aac=true \
+media.stagefright.enable-fma2dp=true \
+media.stagefright.enable-http=true \
+media.stagefright.enable-player=true \
+media.stagefright.enable-qcp=true \
+media.stagefright.enable-scan=true \
+media.stagefright.thumbnail.prefer_hw_codecs=true \
+ro.config.media_vol_steps=30
 
 # Netflix
 PRODUCT_SYSTEM_PROPERTIES += \
