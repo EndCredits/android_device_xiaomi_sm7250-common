dbefd8b7d45f2bfadfeb5be42b1b4b9452a9f112
EndCredits <alicization.han@gmail.com>
2022-12-18 08:56:57 +0800

    sm7250-common: Build bt host if package from source
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- lito.mk
+++ lito.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_PACKAGES += \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor
+    
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
