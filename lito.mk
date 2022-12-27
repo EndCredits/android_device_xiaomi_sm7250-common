35c6b14e7fbddc58a7fd9bcefebe4fa31cc39c53
EndCredits <alicization.han@gmail.com>
2022-12-27 10:16:11 +0800

    sm7250-common: Also set speed profile for Settings app
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- lito.mk
+++ lito.mk
@@ -369,6 +369,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Preopt SystemUI.
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += Settings
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
