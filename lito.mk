8922fd79c60277326c2818ae66c389329b543a9c
EndCredits <alicization.han@gmail.com>
2022-12-27 10:14:49 +0800

    sm7250-common: Import dexopt speed profile apps from redbull
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- lito.mk
+++ lito.mk
@@ -366,6 +366,14 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
+# Preopt SystemUI.
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+
+# Compile SystemUI on device with `speed`.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.systemuicompilerfilter=speed
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
